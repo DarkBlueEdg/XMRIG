01d1fec9f3fff90d6cb2f37b74b30fb7560630e3
TryOwnMe <44828851+DarkBlueEdg@users.noreply.github.com>
2022-06-16 16:57:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ time1="$( date +"%R" )"
 
 msgxmrig() { echo -e "${*} \x1b[38;5;214m[${time1}]\e[0m \x1b[31;5;227m[STARTING XMRIG]:\e[0m\x1b[38;5;87m XMRIG\n" >&1;:;}
 
-cd xmrig/build
+cd xmrigocean/build
 
 msgxmrig
 ./xmrig
